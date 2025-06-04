4e9ff45f1da9e90cc0ab2d69f9617ba78c5c49fd
Юрий Исиметов <yur78bz@gmail.com>
2025-06-04 09:25:11 +0500

    Добавьте файлы проекта.

---
+++ WFShahMat/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WFShahMat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtRooks3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRooks2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRooks1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKing = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lbMessage = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtRooks3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtRooks2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtRooks1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtKing);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(193, 116);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtRooks3
+            // 
+            this.txtRooks3.Location = new System.Drawing.Point(134, 82);
+            this.txtRooks3.Name = "txtRooks3";
+            this.txtRooks3.Size = new System.Drawing.Size(50, 20);
+            this.txtRooks3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Третья ладья:";
+            // 
+            // txtRooks2
+            // 
+            this.txtRooks2.Location = new System.Drawing.Point(134, 56);
+            this.txtRooks2.Name = "txtRooks2";
+            this.txtRooks2.Size = new System.Drawing.Size(50, 20);
+            this.txtRooks2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Вторая ладья:";
+            // 
+            // txtRooks1
+            // 
+            this.txtRooks1.Location = new System.Drawing.Point(134, 30);
+            this.txtRooks1.Name = "txtRooks1";
+            this.txtRooks1.Size = new System.Drawing.Size(50, 20);
+            this.txtRooks1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Первая ладья:";
+            // 
+            // txtKing
+            // 
+            this.txtKing.Location = new System.Drawing.Point(134, 4);
+            this.txtKing.Name = "txtKing";
+            this.txtKing.Size = new System.Drawing.Size(50, 20);
+            this.txtKing.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Расположение короля:";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(13, 136);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(193, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Проверить";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.AutoSize = true;
+            this.lbMessage.Location = new System.Drawing.Point(13, 166);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(58, 13);
+            this.lbMessage.TabIndex = 2;
+            this.lbMessage.Text = "lbMessage";
+            this.lbMessage.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCheck;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 191);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(235, 230);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Проверка боя короля";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtRooks1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRooks3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRooks2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lbMessage;
+    }
+}
+
---
+++ WFShahMat/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFShahMat
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        private void txtKing_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string king=txtKing.Text.Trim(); 
+            string[] rooks = new string[3];
+            rooks[0] = txtRooks1.Text.Trim();
+            rooks[1] = txtRooks2.Text.Trim();
+            rooks[2] = txtRooks3.Text.Trim();
+
+            // проверка корректности ввода координат короля
+            if (!IsValidPosition(king))
+            {
+                MessageBox.Show("Не кооректные координаты короля. Введите значение в формате a1-h8");
+                return;
+            }
+
+            //  проверка корректности ввода координат ладей
+            foreach (var rook in rooks)
+            {
+                if (!IsValidPosition(rook))
+                {
+                    MessageBox.Show($"Не кооректные координаты ладьи. Введите значение в формате a1-h8");
+                    return;
+                }
+            }
+            // проверяем находится ли король под боем
+            List<string> threats = new List<string>();
+
+            foreach (var rook in rooks)
+            {
+                if (IsUnderThreat(king, rook))
+                {
+                    threats.Add(rook);
+                }
+
+                // Выводим результат
+                if (threats.Count>0)
+                {
+                    lbMessage.Visible = true;
+                    lbMessage.Text = "Король под боем от ладей: " + string.Join(", ", threats);
+                }
+                else
+                {
+                    lbMessage.Visible = true;
+                    lbMessage.Text = "Король не под боем";
+                }
+            }
+        }
+
+        //Метод для проверки,находится ли король под угрозой ладьи
+        private bool IsUnderThreat(string king, string rook)
+        {
+            // проверяем что координаты корректны
+            if (king.Length!=2|| rook.Length!=2)
+
+                return false;
+            char kingFile = king[0]; // a-h
+            char kingRank = king[1]; // 1-8
+            char rookFile = rook[0]; // a-h
+            char rookRank = rook[1]; // 1-8
+
+            //проверяем находятся ли король и ладья на одной линии
+
+            return (kingFile == rookFile || kingRank == rookRank);            
+        }
+
+        // метод для проверки корректности ввода координат
+        private bool IsValidPosition(string position)
+        {
+            // проверяется что длина строки равна 2
+            if (position.Length != 2) return false;
+            
+            char file = position[0]; // первая буква (a-h)
+
+            char rank = position[1]; // вторая цифра (1-8)
+
+            // проверяется что первая буква находится в диапазоне от "а" до "h" и 
+            // второе значение в диапазоне от 1 до 8
+            return (file >= 'a' && file <='h' && rank>='1' && rank <='8');
+        }  
+    }
+}
